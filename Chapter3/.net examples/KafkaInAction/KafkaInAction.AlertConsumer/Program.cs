871346a8373d1c3d466dff7d83831aafd9ea5069
Nikolai Fedorov <nikolayfedorovv@yandex.ru>
2024-01-05 00:08:13 +0300

    Реализовал на CSharp поставищика и потребителя данных из главы 3

--- Chapter3/.net examples/KafkaInAction/KafkaInAction.AlertConsumer/Program.cs
+++ Chapter3/.net examples/KafkaInAction/KafkaInAction.AlertConsumer/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using org.kafkainaction;
+
+string bootstrapServers = "localhost:9092";
+string schemaRegistryUrl = "http://localhost:8081";
+string topicName = "kinaction_schematest";
+string groupName = "kinaction-schematest";
+
+CancellationTokenSource cts = new();
+
+
+    using (CachedSchemaRegistryClient schemaRegister =
+        new(new SchemaRegistryConfig
+        {
+            Url = schemaRegistryUrl
+        }))
+using (var consumer =
+    new ConsumerBuilder<long, Alert>(new ConsumerConfig
+    {
+        BootstrapServers = bootstrapServers,
+        GroupId = groupName
+    })
+    .SetValueDeserializer(new AvroDeserializer<Alert>(schemaRegister).AsSyncOverAsync())
+        .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+        .Build())
+    {
+        consumer.Subscribe(topicName);
+
+        try
+        {
+            while (true)
+            {
+                try
+                {
+                    var consumeResult = consumer.Consume(cts.Token);
+                    var alert = consumeResult.Message.Value;
+                    Console.WriteLine($"kinaction_info offset = {consumeResult.Offset},\r\nConsume Alert:\r\n{AlertToString(alert)}");
+                    Console.WriteLine();
+                }
+                catch (ConsumeException e)
+                {
+                    Console.WriteLine($"Consume error: {e.Error.Reason}");
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumer.Close();
+        }
+    }
+
+
+
+
+string AlertToString(Alert alert)
+{
+    string result = $"sensor_id = {alert.sensor_id}\r\ntime={alert.time}\r\nstatus={alert.status}";
+    return result;
+}
--- Chapter3/.net examples/KafkaInAction/KafkaInAction.AlertProducer/Program.cs
+++ Chapter3/.net examples/KafkaInAction/KafkaInAction.AlertProducer/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using org.kafkainaction;
+
+string bootstrapServers = "localhost:9092";
+string schemaRegistryUrl = "http://localhost:8081";
+string topicName = "kinaction_schematest";
+
+
+var alertStatusArray = Enum.GetValues(typeof(AlertStatus)).Cast<AlertStatus>().ToArray();
+
+Random random = new ();
+
+
+using (CachedSchemaRegistryClient schemaRegistry =
+    new(new SchemaRegistryConfig { Url = schemaRegistryUrl }))
+using(var producer = 
+    new ProducerBuilder<long, Alert>(new ProducerConfig
+    {
+        BootstrapServers = bootstrapServers,
+        
+    })
+    .SetValueSerializer(new AvroSerializer<Alert>(schemaRegistry, new AvroSerializerConfig
+    {
+         BufferBytes = 100
+    }))
+    .Build())
+{
+    Console.WriteLine("Enter any text to send Alert into Kafka or enter q to exit");
+    
+    while(Console.ReadLine() != "q")
+    {
+        int statusIndex = random.Next(0, alertStatusArray.Length);
+        Alert alert = new()
+        {
+            sensor_id = 12345L,
+            status = alertStatusArray[statusIndex],
+            time = DateTimeOffset.Now.ToUnixTimeSeconds()
+        };
+
+        Message<long, Alert> producerRecord = new()
+        {
+            Key = alert.sensor_id,
+            Value = alert
+        };
+
+        await producer.ProduceAsync(topicName, producerRecord);
+
+        Console.WriteLine($"Sended Alert:\r\n{AlertToString(alert)}");
+    }
+}
+
+string AlertToString(Alert alert)
+{
+    string result = $"sensor_id = {alert.sensor_id}\r\ntime={alert.time}\r\nstatus={alert.status}";
+    return result;
+}
+
+
